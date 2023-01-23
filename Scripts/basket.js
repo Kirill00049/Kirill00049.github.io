092c6dee72eacc8925fb14995a80720e9d78d904
Kirill00049 <97191260+Kirill00049@users.noreply.github.com>
2023-01-24 01:30:06 +0300

    Adaptability version 1.1

--- Scripts/basket.js
+++ Scripts/basket.js
@@ -4,7 +4,14 @@ let btnsMinus=document.getElementsByClassName('less')
 for(let i=0; i<btnsPlus.length; i++){
     btnsPlus[i].addEventListener('click', function(event){
         let divQuantity = btnsPlus[i].parentNode.parentNode.getElementsByClassName('quantity')[0]
-        let q= parseInt(divQuantity.innerHTML) + 1
+        let q
+        if (Math.floor(divQuantity.innerHTML % 7) == 5){
+            q = parseInt(divQuantity.innerHTML) + 2
+        }
+        else{
+            q = parseInt(divQuantity.innerHTML) + 1
+        }
+        
         divQuantity.innerHTML= q
         let newPrice=q*price-Math.floor(q / 7)*price
         btnsPlus[i].parentNode.getElementsByClassName('price')[0].innerHTML=newPrice + ' &#x20bd'
