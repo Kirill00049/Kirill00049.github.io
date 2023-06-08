d15d3a58decbdc36900efa34a0b969c6ad61d90e
Kirill00049 <97191260+Kirill00049@users.noreply.github.com>
2023-06-08 21:04:28 +0300

    Add files via upload

--- Scripts/promo.js
+++ Scripts/promo.js
@@ -1,24 +1,18 @@
-///---------слайдер
-/* Индекс слайда по умолчанию */
 var slideIndex = 1;
 console.log(window.outerWidth);
 
 if(window.outerWidth>300){
     showSlides(slideIndex);
 }
-/* Функция увеличивает индекс на 1, показывает следующй слайд*/
 function plusSlide() {
     showSlides(slideIndex += 1);
 }
-/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
 function minusSlide() {
     showSlides(slideIndex -= 1);  
 }
-/* Устанавливает текущий слайд */
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
-/* Основная функция слайдера */
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("item");
